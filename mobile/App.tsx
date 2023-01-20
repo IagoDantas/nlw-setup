35c2bc10a42e3039abb3d2fe2aecbfbe8fc3f9c2
Iago Dantas <iagodantas.8@gmail.com>
2023-01-20 12:50:20 -0300

    home screen mobile and add some utils folders

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import {
   useFonts,
@@ -7,7 +7,9 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter'
+
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
 
@@ -16,30 +18,20 @@ export default function App() {
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold
-  })
+  });
+
   if (!fontsLoaded) {
     return
-    (
+    {
       <Loading />
-    );
+    };
   }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar backgroundColor='transparent' barStyle="light-content" translucent />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
