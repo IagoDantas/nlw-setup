34c8a14a8686adc2f9245770812cefad1d032379
Iago Dantas <iagodantas.8@gmail.com>
2023-01-20 09:17:16 -0300

    routes of post habits and get days

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,12 @@
 //Back-end API RESTful
 import Fastify from "fastify";
-import {PrismaClient} from "@prisma/client";
 import cors from '@fastify/cors';
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors)
-
-app.get('/hello', async ()=>{
-    const habits = await prisma.habit.findMany();
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
