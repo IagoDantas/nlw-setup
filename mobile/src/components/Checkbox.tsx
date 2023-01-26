99e2227b25d19481f3f536f5a051a91aec40f9a5
Iago Dantas <iagodantas.8@gmail.com>
2023-01-26 09:55:55 -0300

    checkbox & progressbar animated

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -2,7 +2,7 @@ import { TouchableOpacity, TouchableOpacityProps, View, Text } from "react-nativ
 
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
-
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 interface Props extends TouchableOpacityProps {
     checked?: boolean;
     title: string
@@ -13,13 +13,18 @@ export function Checkbox({ checked = false, title, ...rest }: Props) {
     return (
         <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center" {...rest}>
             {checked ?
-                (<View className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'>
-                    <Feather
-                        name="check"
-                        size={20}
-                        color={colors.white}
-                    />
-                </View>
+                (
+                    <Animated.View
+                        className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'
+                        entering={ZoomIn}
+                        exiting={ZoomOut}
+                    >
+                        <Feather
+                            name="check"
+                            size={20}
+                            color={colors.white}
+                        />
+                    </Animated.View>
                 )
 
                 :
--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useRoute, useFocusEffect } from '@react-navigation/native'
+import { useCallback, useState } from "react";
 import { api } from "../lib/axios";
 import { ScrollView, View, Text, Alert } from "react-native";
-import { useRoute } from '@react-navigation/native'
 import { BackButton } from "../components/BackButton";
 
 import dayjs from 'dayjs'
 import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
 import { Loading } from "../components/Loading";
+import { HabitsEmpty } from '../components/HabitsEmpty'
 import { generateProgressPercentage } from '../utils/generate-progress-percentage'
+import clsx from "clsx";
 interface Params {
     date: string;
 }
@@ -34,6 +36,8 @@ export function Habit() {
     const habitsProgress = dayInfo?.possibleHabits.length ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) : 0
 
     const parsedDate = dayjs(date)
+    const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
+
     const dayOfWeek = parsedDate.format('dddd')
     const dayOfMonth = parsedDate.format('DD/MM')
 
@@ -52,17 +56,25 @@ export function Habit() {
         }
     }
     async function handleToggleHabit(habitId: string) {
-        if (completedHabits.includes(habitId)) {
-            setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId))
+        try {
+            await api.patch(`/habits/${habitId}/toggle`)
+            if (completedHabits.includes(habitId)) {
+                setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId))
+            }
+            else {
+                setCompletedHabits(prevState => [...prevState, habitId])
+            }
         }
-        else {
-            setCompletedHabits(prevState => [...prevState, habitId])
+        catch (error) {
+            console.log(error)
+            Alert.alert('Ops', 'Não foi possível atualizar o hábito')
         }
     }
 
-    useEffect(() => {
-        fetchHabits()
-    }, [])
+
+    useFocusEffect(useCallback(() => {
+        fetchHabits();
+    }, []));
 
 
     if (loading) {
@@ -87,18 +99,28 @@ export function Habit() {
                 </Text>
                 <ProgressBar progress={habitsProgress} />
 
-                <View className="mt-6">
-                    {dayInfo?.possibleHabits &&
+                <View className={clsx("mt-6", {
+                    ['opacity-50']: isDateInPast
+                })}>
+                    {dayInfo?.possibleHabits ?
                         dayInfo.possibleHabits.map(habit => (
                             <Checkbox
                                 key={habit.id}
                                 title={habit.title}
                                 checked={completedHabits.includes(habit.id)}
+                                disabled={isDateInPast}
                                 onPress={() => handleToggleHabit(habit.id)}
                             />
                         ))
+                        : <HabitsEmpty />
                     }
                 </View>
+                {
+                    isDateInPast && (
+                        <Text className="text-white mt-10 text-center">
+                            Você não pode editar hábitos de uma data passada
+                        </Text >)
+                }
             </ScrollView>
         </View>
     )
